98314bf0d2658fab0170db74871fc62967fd7831
Amila Upeksha Abeyrathne <amilaupeksha@outlook.com>
2017-11-30 17:28:56 +0530

    changed

--- Virtusa.SocialPlatform/Virtusa.SocialPlatform/ClientApp/app/components/home/home.component.ts
+++ Virtusa.SocialPlatform/Virtusa.SocialPlatform/ClientApp/app/components/home/home.component.ts
@@ -1,15 +1,60 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HomeService } from "./home.service";
+
+
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
-    providers: [HomeService],
+    providers: [HomeService]
 })
 export class HomeComponent {
 
-    constructor(private competitionService: CompetitionService/*private popup: Popup*/) {
-        this.competition = new Competition();
+    constructor(private homeService: HomeService, private router: Router) {
     }
 
+    ngOnInit(): void {
+        debugger;
+        this.homeService
+            .login()
+            .then(result => {
+                if (result.State == 1) {
+                    this.router.navigate(["./home"]);
+                }
+                else {
+                    alert(result.Msg);
+                }
+            });
+    }
 }
+//import { Component } from '@angular/core';
+//import { HomeService } from "./home.service";
+//import { Router } from '@angular/router';
+
+//@Component({
+//    selector: 'home',
+//    templateUrl: './home.component.html',
+//    styleUrls: ['./home.component.css'],
+//    providers: [HomeService],
+//})
+//export class HomeComponent {
+
+//    constructor(private homeService: HomeService, private router: Router) {
+//    }
+
+//    //ngOnInit(): void {
+//    //    debugger;
+//    //    this.homeService
+//    //        .login()
+//    //        .then(result => {
+//    //            if (result.State == 1) {
+//    //                this.router.navigate(["./home"]);
+//    //            }
+//    //            else {
+//    //                alert(result.Msg);
+//    //            }
+//    //        });
+//    //}
+//}
